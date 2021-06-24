90a92f4d721adab42c7a260fa09c197c795388cb
Julia Brunett <julia.h.brunett@gmail.com>
2021-06-24 10:01:49 -0700

    Working on bubble plot

--- static/js/app.js
+++ static/js/app.js
@@ -22,40 +22,41 @@ d3.json("../../data/samples.json").then((data) => {
     console.log(ids);
 
      // Create an array for values
-    // values = data.samples.map(d => d.sample_values);
-    values = sortedValues.map(d => d.sample_values.slice(0,10).reverse());
+    values = sortedValues.map(d => d.sample_values);
+    top_10_values = sortedValues.map(d => d.sample_values.slice(0,10).reverse());
     console.log("---Values---");
-    console.log(values[0]);
+    console.log(top_10_values[0]);
 
      // Create an array for otu ids
-    // otu_ids = data.samples.map(d => d.otu_ids);
-    otu_ids = sortedValues.map(d => d.otu_ids.slice(0,10).reverse());
+    otu_ids = sortedValues.map(d => d.otu_ids);
+    top_10_otu_ids = sortedValues.map(d => d.otu_ids.slice(0,10).reverse());
     console.log("---OTU IDs---");
-    console.log(otu_ids[0]);
+    console.log(top_10_otu_ids[0]);
 
     // Initialize an array
     var string_otu_ids = [];
     
     // Loop through to convert int ids to strings
-    for (var i = 0; i < otu_ids.length; i++) {
-        string_otu_ids[i] = otu_ids[i].map(String);
+    for (var i = 0; i < top_10_otu_ids.length; i++) {
+        string_otu_ids[i] = top_10_otu_ids[i].map(String);
     };
     console.log("---String OTU IDs---");
     console.log(string_otu_ids[0]);
 
      // Create an array for otu labels
-    // otu_labels = data.samples.map(d => d.otu_labels);
-    otu_labels = sortedValues.map(d => d.otu_labels.slice(0,10).reverse());
+    otu_labels = sortedValues.map(d => d.otu_labels);
+    top_10_otu_labels = sortedValues.map(d => d.otu_labels.slice(0,10).reverse());
     console.log("---OTU Labels---");
-    console.log(otu_labels[0]);
+    console.log(top_10_otu_labels[0]);
 
     // Initialize the graph when loaded with default data
     function init() {
     
+        // Bar Plot
         var trace1 = [{
-            x: values[0],
+            x: top_10_values[0],
             y: string_otu_ids[0],
-            text: otu_labels[0],
+            text: top_10_otu_labels[0],
             type: "bar",
             orientation: "h"
         }];
@@ -70,12 +71,46 @@ d3.json("../../data/samples.json").then((data) => {
                 type: "category"
             }
         };
-    
-        var plot = d3.selectAll("#plot").node()
 
-        Plotly.newPlot(plot, trace1, layout1);
+        // Bubble Plot
+        var trace2 = [{
+            x: otu_ids[0],
+            y: values[0],
+            text: otu_labels[0],
+            mode: 'markers',
+            marker: {
+            color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+            size: values[0]
+            }
+        }];
+        
+        
+        var layout2 = {
+            title: 'Bubble Chart Hover Text',
+            showlegend: false,
+            height: 600,
+            width: 1000,
+            xaxis: {
+                title: "OTU ID"
+            },
+            yaxis: {
+                title: "Sample Values",
+                //type: "category"
+            }
+        };
+        
+        
+        
+        // Define where the plots will live
+        var bar_plot = d3.selectAll("#bar-plot").node();
+        var bubble_plot = d3.selectAll("#bubble-plot").node();
+
+        // Plot the plots
+        Plotly.newPlot(bar_plot, trace1, layout1);
+        Plotly.newPlot(bubble_plot, trace2, layout2);
     
     };
+
 
     // Select the dropdown menu
     var dropdownMenu = d3.select("#selID");
@@ -86,6 +121,7 @@ d3.json("../../data/samples.json").then((data) => {
         option.text(ids[x]).attr("value", `${ids[x]}`);
     };
 
+    // When the page is changed, update the plot
     d3.selectAll("select").on("change", updatePlotly);
 
 // Function when a dropdown option is chosen
@@ -118,12 +154,12 @@ function updatePlotly() {
         };
     };
 
-    var plot = d3.selectAll("#plot").node()
+    var bar_plot = d3.selectAll("#bar-plot").node()
 
-    Plotly.update(plot, layout_update);
-    Plotly.restyle(plot, "x", [x]);
-    Plotly.restyle(plot, "y", [y]);
-    Plotly.restyle(plot, "text", [text]);
+    Plotly.update(bar_plot, layout_update);
+    Plotly.restyle(bar_plot, "x", [x]);
+    Plotly.restyle(bar_plot, "y", [y]);
+    Plotly.restyle(bar_plot, "text", [text]);
     
 
     console.log(dataset);
